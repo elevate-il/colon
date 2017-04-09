cda0fe180d7e6b144deaaad0d7d4c00f99ab660b
elevate-il <etai@Elevate.co.il>
2017-04-09 11:48:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+ $('.checkbox').click(function(){
+
+
+  $(this).toggleClass("on");
+
+})
+ $('.button').click(function(){
+
+    if($('.on').length==2){
+        
+        window.location='14573_form_a4_c.pdf';
+    }else{
+        swal('נא לאשר תקנון ושאתם מעל גיל 18')
+    }
+})
+})
